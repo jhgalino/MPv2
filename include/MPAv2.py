5c882960a15e5fcbeaaaec5fa20a02e7d701775c
John Henry Galino <jhgalino@gmail.com>
2020-07-06 11:52:27 +0800

    missing final friend

--- include/MPAv2.py
+++ include/MPAv2.py
@@ -1,101 +1,92 @@
-# names[index][0] is name, [1] is score, [2] is distance
-def checkIfSameScore(names: list):
-    n = 1
-    while n <= len(names) - 1:
-        if int(names[n][1]) == int(names[n - 1][1]):
-            return True
-            break
-        n += 1
-    return False
+class RelationshipCircle:
+    def __init__(self, listOfNames: list, centerScore: int, size: int):
+        self.listOfNames = listOfNames
+        self.centerScore = centerScore
+        self.size = size
+        self.barkada = []
+
+    def addDistance(self):
+        for name in self.listOfNames:
+            distance = abs(self.centerScore - int(name[1]))
+            name.append(distance)
+
+    def sortByDistance(self):
+        self.listOfNames = sorted(self.listOfNames, key=lambda x: x[2])
+        print(self.listOfNames)
+
+    def sortByScore(self):
+        for n in range(1, len(self.listOfNames)):
+            position = n
+            current = self.listOfNames[position]
+            previous = self.listOfNames[position - 1]
+            while current[2] == previous[2]:
+                if current[1] < previous[1]:
+                    tempHolder = current
+                    self.listOfNames.pop(position)
+                    self.listOfNames.insert(position - 1, tempHolder)
+                    position -= 1
+                    try:
+                        current = self.listOfNames[position]
+                        previous = self.listOfNames[position - 1]
+                    except Exception:
+                        break
+                else:
+                    break
+        print(self.listOfNames)
+
+    def sortByName(self):
+        for n in range(1, len(self.listOfNames)):
+            position = n
+            current = self.listOfNames[position]
+            previous = self.listOfNames[position - 1]
+            while current[1] == previous[1]:
+                if len(current[0]) < len(previous[0]):
+                    tempHolder = current
+                    self.listOfNames.pop(position)
+                    self.listOfNames.insert(position - 1, tempHolder)
+                    position -= 1
+                    try:
+                        current = self.listOfNames[position]
+                        previous = self.listOfNames[position - 1]
+                    except Exception:
+                        break
+                elif current[0] > previous[0]:
+                    tempHolder = current
+                    self.listOfNames.pop(position)
+                    self.listOfNames.insert(position - 1, tempHolder)
+                    position -= 1
+                    try:
+                        current = self.listOfNames[position]
+                        previous = self.listOfNames[position - 1]
+                    except Exception:
+                        break
+                else:
+                    break
+        print(self.listOfNames)
+
+    def addToBarkada(self):
+        for name in self.listOfNames:
+            if len(self.barkada) != self.size:
+                self.barkada.append(name)
 
 
-def addDistance(listOfNames: list, score: int):
-    for i in listOfNames:
-        distance = abs(int(score) - int(i[1]))
-        i.append(distance)
-    return listOfNames
-
-
-# check if name earlier in list is longer
-def checkLongerName(name1: str, name2: str):
-    if len(name1) > len(name2):
-        return True
-    else:
-        return False
-
-
-# names[index][0] is name, [1] is score, [2] is distance
-def sortByDistance(listOfNames: list):
-    listOfNames = sorted(listOfNames, key=lambda x: (x[2], x[1]))
-    index = 1
-    while index <= len(listOfNames) - 1:  # check if name earlier in list is longer
-        if listOfNames[index][1] == listOfNames[index - 1][1]:
-            if checkLongerName(listOfNames[index - 1][0], listOfNames[index][0]):
-                # swap
-                tempHolder = listOfNames[index - 1]
-                listOfNames[index - 1] = listOfNames[index]
-                listOfNames[index] = tempHolder
-            else:
-                # use reverse lexicographical order
-                if listOfNames[index][0] > listOfNames[index - 1][0]:
-                    tempHolder = listOfNames[index - 1]
-                    listOfNames[index - 1] = listOfNames[index]
-                    listOfNames[index] = tempHolder
-        index += 1
-    return listOfNames
-
-
-def sortByDistance2(listOfNames: list):
-    listOfNames = sorted(listOfNames, key=lambda x: (x[3], x[1]))
-    index = 1
-    while index <= len(listOfNames) - 1:  # check if name earlier in list is longer
-        if listOfNames[index][1] == listOfNames[index - 1][1]:
-            if checkLongerName(listOfNames[index - 1][0], listOfNames[index][0]):
-                # swap
-                tempHolder = listOfNames[index - 1]
-                listOfNames[index - 1] = listOfNames[index]
-                listOfNames[index] = tempHolder
-            else:
-                # use reverse lexicographical order
-                if listOfNames[index][0] > listOfNames[index - 1][0]:
-                    tempHolder = listOfNames[index - 1]
-                    listOfNames[index - 1] = listOfNames[index]
-                    listOfNames[index] = tempHolder
-        index += 1
-    return listOfNames
-
-
-# expectedFriend[0] is name, [1] is score, [2] is distance
-def findExtraFriend(names: list, scoreOfFriend: int, expectedCloseness: int):
-    newNames = addDistance(names, scoreOfFriend)
-    newNames = sortByDistance2(newNames)
-    expectedFriend = newNames[1]
-    if expectedFriend[3] == expectedCloseness:
-        return expectedFriend[0]
-    else:
-        return "almost"
-
-
-listNames = []
-actualBarkada = []
+names = []
 score = int(input())
 barkadaSize = int(input())
 numberOfNames = int(input())
 for n in range(numberOfNames):
-    listNames.append(input().split())
+    name = input().split()
+    name[1] = int(name[1])
+    names.append(name)
 
-tempBarkada = addDistance(listNames, score)
-barkadaList = sortByDistance(tempBarkada)
+relCircle1 = RelationshipCircle(names, score, barkadaSize)
+relCircle1.addDistance()
+relCircle1.sortByDistance()
+relCircle1.sortByScore()
+relCircle1.sortByName()
+relCircle1.addToBarkada()
 
-for n in range(barkadaSize):
-    actualBarkada.append(barkadaList[n][0])
-almostBarkada = barkadaList[barkadaSize]
-extraFriend = findExtraFriend(listNames, almostBarkada[1], barkadaList[0][2])
-if extraFriend in actualBarkada:
-    actualBarkada.append(almostBarkada[0])
-    for name in actualBarkada:
-        print(name)
-else:
-    for name in actualBarkada:
-        print(name)
-    print("{}, {}".format(almostBarkada[0], extraFriend))
+
+extra = relCircle1.listOfNames[barkadaSize]
+for i in relCircle1.barkada:
+    print(i[0])
