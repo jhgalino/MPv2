c48858a04f17eeafe655b635c446107df9cc19f2
John Henry Galino <jhgalino@gmail.com>
2020-07-25 15:31:29 +0800

    random stuff

--- include/MPG.py
+++ include/MPG.py
@@ -1,17 +1,27 @@
+from memory_profiler import profile
+
 arrayOfSubArray = []
 numberOfID = int(input())
-ids = list(map(int, input().split()))
+ids = [int(x) for x in input().split()]
 
 
-for n in range(2, numberOfID + 1):
-    x = 0
-    while x + n != numberOfID + 1:
-        arrayOfSubArray.append(sorted(ids[x : x + n])[:2])
-        x += 1
+@profile
+def func():
+    highestValue = 0
+    for n in range(2, numberOfID + 1):
+        x = 0
+        while x + n != numberOfID + 1:
+            toSrt = ids[x : x + n]
+            toSrt.sort()
+            arrayOfSubArray.append(toSrt[:2])
+            x += 1
 
-values = sorted(
-    list(
-        map(lambda x: ((x[0] & x[1]) ^ (x[0] ^ x[1])) | (x[0] ^ x[1]), arrayOfSubArray)
-    )
-)
-print(values[-1])
+    for x in arrayOfSubArray:
+        currentValue = ((x[0] & x[1]) ^ (x[0] ^ x[1])) | (x[0] ^ x[1])
+        if currentValue > highestValue:
+            highestValue = int(currentValue)
+
+    print(highestValue)
+
+
+func()
